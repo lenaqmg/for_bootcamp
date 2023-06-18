8d35a901618052e40289f48e53e1011539c6a1f8
lenaqmg <lenaqmg2015@gmail.com>
2023-06-18 15:17:43 +0300

    clean

--- DI-Bootcamp-Stage1/Final-Project/script.js
+++ DI-Bootcamp-Stage1/Final-Project/script.js
@@ -1,7 +1,4 @@
 function changeText(src) {
     var iframe = document.getElementById('iframe1'); 
     iframe.src = src;
-    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-    var textElement = iframeDoc.getElementById('text');
-    textElement.textContent = 'Новый текст на странице';
   }
